492739259521e122acb2092645950695d426db25
traffic95 <traffic95@xeondev.com>
2025-09-24 12:44:15 +0200

    CNBetaWin2.3.4 support

--- src/network.zig
+++ src/network.zig
@@ -20,7 +20,6 @@ pub fn init(allocator: zz.ChunkAllocator) void {
 
     _ = root.intercept(allocator, @intFromPtr(getaddrinfo), GetaddrinfoHook);
     _ = root.intercept(allocator, root.base + root.offsets.unwrapOffset(.MAKE_INITIAL_URL), MakeInitialUrlHook);
-    _ = root.intercept(allocator, root.base + root.offsets.unwrapOffset(.SYSTEM_URI_CREATE_THIS), SystemUriHook);
 }
 
 const MakeInitialUrlHook = struct {
@@ -57,33 +56,6 @@ const GetaddrinfoHook = struct {
             return @This().originalFn(null_address.ptr, service_name, hints, result);
         } else {
             return @This().originalFn(node_name, service_name, hints, result);
-        }
-    }
-};
-
-const SystemUriHook = struct {
-    pub var originalFn: *const fn (usize, usize, u8, u32) callconv(.c) usize = undefined;
-    const cn_asset_path_utf8 = "StandaloneWindows64/cn/";
-    const oversea_asset_path_utf8 = "StandaloneWindows64/oversea/";
-    const cn_asset_path_utf16 = unicode.utf8ToUtf16LeStringLiteral(cn_asset_path_utf8);
-
-    pub fn callback(this: usize, url: usize, a2: u8, a3: u32) callconv(.c) usize {
-        var temp: [2048]u8 = undefined;
-
-        const str = util.readCSharpString(url);
-        if (std.mem.indexOf(u16, str, cn_asset_path_utf16) != null) {
-            const length = unicode.utf16LeToUtf8(&temp, str) catch return @This().originalFn(this, url, a2, a3);
-            const path_index = std.mem.indexOf(u8, temp[0..length], cn_asset_path_utf8).?;
-
-            const length_diff = oversea_asset_path_utf8.len - cn_asset_path_utf8.len;
-            @memmove(temp[path_index + length_diff .. length + length_diff], temp[path_index..length]);
-            @memcpy(temp[path_index .. path_index + oversea_asset_path_utf8.len], oversea_asset_path_utf8);
-            temp[length + length_diff] = 0;
-
-            std.log.debug("replaced: {s}", .{temp[0 .. length + length_diff]});
-            return @This().originalFn(this, util.ptrToStringAnsi(temp[0 .. length + length_diff]), a2, a3);
-        } else {
-            return @This().originalFn(this, url, a2, a3);
         }
     }
 };
