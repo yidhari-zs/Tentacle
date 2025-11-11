fc9a040210941d63b013227eaa19fa3896940221
traffic95 <traffic95@xeondev.com>
2025-11-12 00:30:15 +0100

    CNBetaWin2.5.0 support

--- src/root.zig
+++ src/root.zig
@@ -52,7 +52,7 @@ fn onAttach() void {
     ) catch {};
 
     std.log.debug("Successfully injected. Waiting for the game startup.", .{});
-    std.log.debug("To work with Yidhari-ZS: https://git.xeondev.com/yidhari-zs/yidhari-zs", .{});
+    std.log.debug("To work with Yidhari-ZS (2.5beta): https://git.xeondev.com/yidhari-zs/Yidhari-ZS/src/branch/2.5beta", .{});
 
     base = while (true) {
         if (windows.kernel32.GetModuleHandleW(game_assembly_name)) |addr| break @intFromPtr(addr);
